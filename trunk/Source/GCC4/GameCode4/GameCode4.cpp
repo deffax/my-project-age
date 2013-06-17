ba92e5476b51197096231cab1b6138d0bea6b5af
deffax <deffax@4d77eb4c-197f-4afd-63e9-ab4792e97ede>
2013-06-17 12:21:29 +0000

    implementazione della classe ZipFile

--- trunk/Source/GCC4/GameCode4/GameCode4.cpp
+++ trunk/Source/GCC4/GameCode4/GameCode4.cpp
@@ -1,6 +1,14 @@
 #include "GameCodeStd.h"
 #include "GameCode.h"
+
+#pragma comment(lib, "shell32.lib")
+#pragma comment(lib, "advapi32.lib")
+
 #pragma comment(lib, "tinyxml.lib")
+#pragma comment(lib, "zlibstat.lib")
+
+
+#include "../ResourceCache/ZipFile.h"
 
 
 INT WINAPI GameCode4(HINSTANCE hInstance,
@@ -8,8 +16,8 @@ INT WINAPI GameCode4(HINSTANCE hInstance,
                      LPWSTR    lpCmdLine,
                      int       nCmdShow)
 {
-	Logger::Init("\\Game\\Logging.xml");
-
+	Logger::Init("Logging.xml");
+	
 	Logger::Destroy();
     return 0;	
 }
