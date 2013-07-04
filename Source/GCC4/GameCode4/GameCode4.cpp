835c653f8acc9220829198c4bcaae89b6ae74bcb
deffax <deffax@4d77eb4c-197f-4afd-63e9-ab4792e97ede>
2013-07-04 08:02:17 +0000

    implementazione minima della classe NWNCloneApp

--- Source/GCC4/GameCode4/GameCode4.cpp
+++ Source/GCC4/GameCode4/GameCode4.cpp
@@ -1,10 +1,6 @@
 #include "GameCodeStd.h"
 #include "GameCode.h"
-//test
-#include "../ResourceCache/ResCache.h"
-#include <string>
-using namespace std;
-//end test
+
 #pragma comment(lib, "shell32.lib")
 #pragma comment(lib, "advapi32.lib")
 
@@ -21,37 +17,8 @@ INT WINAPI GameCode4(HINSTANCE hInstance,
                      int       nCmdShow)
 {
 	Logger::Init("Logging.xml");
-	//test
-	/*char* buffer = NULL;
-	const basic_string <wchar_t> resFileName( L"Assets.zip" );
-	ZipFile zipFile;
-	if(zipFile.Init(resFileName))
-	{
-		optional<int> index = zipFile.Find("Logging.xml");
-		if(index.valid())
-		{
-			int size = zipFile.GetFileLen(*index);
-			buffer = new char[size];
-			if(buffer)
-			{
-				zipFile.ReadFile(*index, buffer);
-			}
-		}
-	}*/
-	const basic_string <wchar_t> resFileName( L"Assets.zip" );
-	ResourceZipFile* zipFile = GCC_NEW ResourceZipFile(resFileName);
-	//ResCache resCache(50, zipFile);
-	ResCache* resCache = GCC_NEW ResCache(50, zipFile);
-	if(resCache->Init())
-	{
-		Resource resource("\\Logger.xml");
-		shared_ptr<ResHandle> handle = resCache->GetHandle(&resource);
-		int size = handle->Size();
-		char *xfile = (char*) handle->Buffer();
-	}
-	// end test
-
-
+	
+	g_pApp->m_Options.Init("PlayerOptions.xml", lpCmdLine);
 	Logger::Destroy();
     return 0;	
 }
--- Source/GCC4/Mainloop/Initialization.h
+++ Source/GCC4/Mainloop/Initialization.h
@@ -3,3 +3,18 @@
 extern bool IsOnlyInstance(LPCTSTR gameTitle);
 extern bool CheckStorage(const DWORDLONG diskSpaceNeed);
 extern DWORD ReadCPUSpeed();
+
+struct GameOptions
+{
+
+    // resource cache options
+    bool m_useDevelopmentDirectories;
+
+	// TiXmlElement - look at this to find other options added by the developer
+	TiXmlDocument *m_pDoc;
+
+	GameOptions();
+	~GameOptions() { SAFE_DELETE(m_pDoc); }
+
+	void Init(const char* xmlFilePath, LPWSTR lpCmdLine);
+};
