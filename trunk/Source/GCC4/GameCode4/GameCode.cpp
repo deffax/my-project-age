18268c9abca4592a4ca9ba96d2e7d1eeb29cef69
deffax <deffax@4d77eb4c-197f-4afd-63e9-ab4792e97ede>
2013-07-12 11:42:27 +0000

    inserimento struttura base per DXUT

--- trunk/Source/GCC4/GameCode4/GameCode.cpp
+++ trunk/Source/GCC4/GameCode4/GameCode.cpp
@@ -1,11 +1,13 @@
 #include "GameCodeStd.h"
 #include "../Mainloop/Initialization.h"
+#include "../ResourceCache/ResCache.h"
 
 GameCodeApp* g_pApp = NULL;
 
 GameCodeApp::GameCodeApp()
 {
 	g_pApp = this;
+	m_resCache = NULL;
 }
 
 bool GameCodeApp::InitInstance(HINSTANCE hInstance, LPWSTR lpCmdLine, HWND hWnd,
@@ -38,8 +40,79 @@ bool GameCodeApp::InitInstance(HINSTANCE hInstance, LPWSTR lpCmdLine, HWND hWnd,
 		resourceCheck = true;
 	}
 
+	IResourceFile *zipFile = GCC_NEW ResourceZipFile(L"Assets.zip");
+	m_resCache = GCC_NEW ResCache(50, zipFile);
 
+	if (!m_resCache->Init())
+	{
+        GCC_ERROR("Failed to initialize resource cache!  Are your paths set up correctly?");
+		return false;
+	}
 	
 
 	return true; //PROVVISORIO!!!!
 }
+
+//--------------------------------------------------------------------------------------
+// Create any D3D9 resources that won't live through a device reset (D3DPOOL_DEFAULT) 
+// or that are tied to the back buffer size 
+//--------------------------------------------------------------------------------------
+HRESULT CALLBACK GameCodeApp::OnD3D9ResetDevice( IDirect3DDevice9* pd3dDevice,
+                                    const D3DSURFACE_DESC* pBackBufferSurfaceDesc, void* pUserContext )
+{
+   
+    return S_OK;
+}
+
+
+
+
+
+
+//--------------------------------------------------------------------------------------
+// Rejects any D3D9 devices that aren't acceptable to the app by returning false
+//--------------------------------------------------------------------------------------
+bool CALLBACK GameCodeApp::IsD3D9DeviceAcceptable( D3DCAPS9* pCaps, D3DFORMAT AdapterFormat,
+                                      D3DFORMAT BackBufferFormat, bool bWindowed, void* pUserContext )
+{
+   
+    return true;
+}
+
+
+
+//--------------------------------------------------------------------------------------
+// Release D3D9 resources created in the OnD3D9ResetDevice callback 
+//--------------------------------------------------------------------------------------
+void CALLBACK GameCodeApp::OnD3D9LostDevice(void* pUserContext )
+{
+   
+}
+
+void CALLBACK GameCodeApp::OnD3D9FrameRender( IDirect3DDevice9* pd3dDevice, double fTime, float fElapsedTime, void* pUserContext  )
+{
+	
+}
+
+
+//--------------------------------------------------------------------------------------
+// Create any D3D9 resources that will live through a device reset (D3DPOOL_MANAGED)
+// and aren't tied to the back buffer size
+//--------------------------------------------------------------------------------------
+HRESULT CALLBACK GameCodeApp::OnD3D9CreateDevice( IDirect3DDevice9* pd3dDevice, const D3DSURFACE_DESC* pBackBufferSurfaceDesc,
+                                     void* pUserContext )
+{
+    
+
+    return S_OK;
+}
+
+
+//--------------------------------------------------------------------------------------
+// Release D3D9 resources created in the OnD3D9CreateDevice callback 
+//--------------------------------------------------------------------------------------
+void CALLBACK GameCodeApp::OnD3D9DestroyDevice( void* pUserContext )
+{
+	
+}
+
